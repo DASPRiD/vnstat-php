28606ac434cfb65501864b6d53014208bbe187f3
Ben Scholzen <mail@dasprids.de>
2015-10-12 21:47:12 +0200

    Fix byte formatting for 32 bit systems

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ function formatBytes($bytes)
     $units = ['B', 'KiB', 'MiB', 'GiB', 'TiB'];
     $pow   = floor(($bytes ? log($bytes) : 0) / log(1024));
     $pow   = min($pow, count($units) - 1);
-    $bytes /= (1 << (10 * $pow));
+    $bytes /= pow(1024, $pow);
 
     return round($bytes) . ' ' . $units[$pow];
 }
